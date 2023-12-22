2ca5a7f715934a87d6a13a3555242e029b623964
Brenda Solben <83696510+solben@users.noreply.github.com>
2023-12-22 19:28:24 +0100

    Crawler completado con los distintos requerimentos solicitados (#3)

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -1,6 +1,12 @@
+import json
+import os
+import re
 from argparse import Namespace
 from queue import Queue
 from typing import Set
+
+import requests
+from bs4 import BeautifulSoup
 
 
 class Crawler:
@@ -8,6 +14,8 @@ class Crawler:
 
     def __init__(self, args: Namespace):
         self.args = args
+        self.url_regex = re.compile(f"^{re.escape(self.args.url)}")
+        self.url_parameters_regex = re.compile(r"\?.*$")
 
     def crawl(self) -> None:
         """Método para crawlear la URL base. `crawl` debe crawlear, desde
@@ -23,11 +31,36 @@ class Crawler:
         - "url": URL de la web
         - "text": Contenido completo (en crudo, sin parsear) de la web
         """
-
         queue: Queue = Queue()
-        queue.put(self.args.url)
-        while ...:
-            ...
+        queue.put(self.args.url)  # url base
+
+        urls_visitadas: set = set()
+
+        while not queue.empty() and len(urls_visitadas) < self.args.max_webs:
+            url = queue.get()  # se extrae una url no visitada
+            urls_visitadas.add(url)
+            response = requests.get(url)  # se extraen todas las urls
+            urls_list = self.find_urls(response.text)
+
+            for url_crawleada in urls_list:
+                if url_crawleada not in urls_visitadas:
+                    queue.put(url_crawleada)
+
+            info_web = {"url": url, "text": response.text}
+
+            url_sin_prefijo = url.removeprefix("https://")
+            directorio_limpio = re.sub(
+                self.url_parameters_regex, "", url_sin_prefijo
+            )
+
+            directorios = os.path.join(
+                self.args.output_folder, directorio_limpio
+            )
+            os.makedirs(directorios, exist_ok=True)
+            web_content = os.path.join(directorios, "content.json")
+
+            with open(web_content, "w") as f:
+                json.dump(info_web, f, indent=4)
 
     def find_urls(self, text: str) -> Set[str]:
         """Método para encontrar URLs de la Universidad Europea en el
@@ -41,5 +74,10 @@ class Crawler:
         Returns:
             Set[str]: conjunto de urls (únicas) extraídas de la web
         """
-        ...
-        return set()
+        soup = BeautifulSoup(text, "html.parser")
+        # re.compile trata a todos los caracteres del href como literales y no especiales.
+        urls_filtradas = [
+            link["href"] for link in soup.find_all("a", href=self.url_regex)
+        ]
+
+        return set(urls_filtradas)
